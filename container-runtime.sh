a0c2f46efd5a882ea1af770410d37c9467dc1cbc
Marvin Korir <60808086+marv254@users.noreply.github.com>
2023-02-10 21:00:51 +0300

    Update container-runtime.sh

--- container-runtime.sh
+++ container-runtime.sh
@@ -16,7 +16,22 @@ EOF
 
 # Apply sysctl params without reboot
 sudo sysctl --system
-sudo apt install containerd
+
+
+#install containerd from apt repo
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt install containerd.io
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 sudo systemctl restart containerd.service
